a1a0756e8e2a65e4ad6ee871234729dab900c54a
chenlong <1583479514@qq.com>
2018-08-06 11:06:50 +0800

    加载完底部提示文案解开

--- src/components/choose-car/src/index.vue
+++ src/components/choose-car/src/index.vue
@@ -11,6 +11,8 @@
           <ul class="sq-brandCars-menu-list">
             <li v-for="(car, inx) in carsData[item]" :key="inx" class="sq-brandCars-menu-item" @click="showModel(car.brandCategoryCode, car.brandCategoryName, item)">
               <div class="sq-brandCars-menu-list-item">
+                <!--作用域插槽， 可传参-->
+                <!--<slot :data="car.brandCategoryCode" class="sq-brandCars-category-img"></slot>-->
                 <img :src="imgConfig[car.brandCategoryCode]" :alt="car.brandCategoryCode" class="sq-brandCars-category-img">
                 <span>{{car.brandCategoryName}}</span>
               </div>
@@ -25,12 +27,14 @@
         <li v-for="(item, index) in brandCategorys" :class="{ 'active': item ===  rightIndex }" :key="index" class="sq-brandCars-category-rightbar-item">{{ item }}</li>
       </ul>
     </div>
+    <!--<select-car v-if="showSelectCar" :brandCategoryData="brandCategoryData" :brandFamilies="selectCar"></select-car>-->
     <!-- +++++++++++++++++++++++++++选车系+++++++++++++++++++++++++++ -->
     <div class="sq-selectcar" v-if="showSelectCar" ref="selectCar" @touchstart="selectCarStarts" @touchmove="selectCarMove" @touchend="selectCarEnd">
       <div class="sq-selectcar-cars-wrapper">
         <div class="sq-selectcar-inner" ref="selectCar">
           <div class="sq-selectcar-slide" ref="slide">
             <h3 class="sq-selectcar-carts-first-title">
+              <!--<slot name="brandCategoryCode"></slot>-->
               <img :src="imgConfig[brandCategoryData.code]" :alt="brandCategoryData.code" class="sq-selectcar-carts-detail-icon">
               <span>{{ brandCategoryData.name }}</span>
             </h3>
@@ -54,12 +58,13 @@
     <div class="sq-selectmodel" v-if="showSelectModel">
       <div class="sq-selectmodel-wrap" ref="selectModel" @touchstart="modelStart" @touchmove="modelMove" @touchend="modelEnd">
         <div class="sq-selectmodel-icon-title">
+          <!--<slot name="selectModelCode"></slot>-->
           <img :src="imgConfig[brandCategoryData.code]" :alt="brandCategoryData.code" class="sq-selectmodel-model-icon">
           <span>{{ brandCategoryData.name }}</span>
         </div>
         <div class="sq-selectmodel-wrapper">
           <div class="sq-selectmodel-inner" ref="wrapper" :style="{ height: wrapperHeight + 'px' }">
-            <sq-loadmore :loading="loading" :bottom-fun="loadMore" :is-finished-load="isFinishedLoad">
+            <sq-loadmore :loading="loading" :bottom-fun="loadMore" :is-finished-load="isFinishedLoad" :bottom-finished-text="finishedText">
               <ul class="sq-selectmodel-ul">
                 <li class="sq-selectmodel-list" v-for="(item, index) in selectModel" :key="index">
                   <h3 class="sq-selectmodel-second-title" v-if="item.carYear">{{ item.carYear }}款</h3>
@@ -76,6 +81,12 @@
                 </li>
               </ul>
             </sq-loadmore>
+
+            <div class="sq-selectmodel-footer" v-if="isShowText">
+              <span class="sq-selectmodel-line-left"></span>
+              <span class="sq-selectmodel-line-text">不好意思， 没有数据了</span>
+              <span class="sq-selectmodel-line-left"></span>
+            </div>
           </div>
         </div>
       </div>
@@ -86,11 +97,17 @@
     <!-- +++++++++++++++++++++++++++++++++搜索车型+++++++++++++++++++++++++++++++++ -->
     <div class="sq-search" v-if="showSearchModal" >
       <div class="sq-search-inner" ref="searchWapper" :style="{ height: searchWapperHeight + 'px' }" @touchstart="searchStart" @touchmove="searchMove" @touchend="searchEnd">
-        <sq-loadmore :loading="searchLoading" :bottom-fun="searchCarLoadMore" :is-finished-load="searchIsFinishedLoad">
+        <sq-loadmore :loading="searchLoading" :bottom-fun="searchCarLoadMore" :is-finished-load="searchIsFinishedLoad" :bottom-finished-text="finishedText">
           <ul class="sq-search-list">
             <li class="sq-search-list-item" v-for="(item, index) in searchCarArr" :key="index" @click.stop="closeSelectModel(item)">{{ item.displayName }}</li>
           </ul>
         </sq-loadmore>
+
+        <div class="sq-selectmodel-footer" v-if="showSearchLoadText">
+          <span class="sq-selectmodel-line-left"></span>
+          <span class="sq-selectmodel-line-text">不好意思， 没有数据了</span>
+          <span class="sq-selectmodel-line-left"></span>
+        </div>
       </div>
     </div>
     <div class="sq-selectmodel-model-modal" v-if="showSearchModal"></div>
@@ -164,7 +181,8 @@ export default {
       searchIsFinishedLoad: false,
       myShowChooseCar: this.showChooseCar,
       scrollArr: [],
-      rightIndex: ''
+      rightIndex: '',
+      finishedText: ''
     }
   },
   watch: {
@@ -175,6 +193,7 @@ export default {
       this.$emit('update:showChooseCar', this.myShowChooseCar)
       this.$emit('praent-event', this.myShowChooseCar)
       if (newVal) {
+        // console.log(this.imgConfig,  'this is a imgConfig')
         this.showSelectCar = false
         this.showSearchModal = false
         this.showSelectModel = false
@@ -338,6 +357,7 @@ export default {
     },
     jumpChooseCar (brandId, familyId) {
       document.querySelector('.sq-brandCars').style.overflow = 'inherit'
+      // this.showSelectCar = false
       this.showSelectModel = true
       this.$emit('brandModelId', { brandId, familyId })
       try {
@@ -670,6 +690,7 @@ export default {
       color: #007EFF;
     }
     &-checked {
+      // display: none;
       position: absolute;
       top: 0;
       right: 15px;
@@ -689,6 +710,7 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
+  // background: #fff;
   z-index: 888;
   animation: fadeInRight 0.8s ease;
   animation-fill-mode: forwards;
@@ -745,6 +767,7 @@ export default {
     list-style: none;
   }
   &-list {
+    // padding-left: 15px;
     word-break: break-all;
   }
   &-second-title {
@@ -774,6 +797,20 @@ export default {
     padding-right: 15px;
     box-sizing: border-box;
     line-height: 1.5em;
+    @include mix-1px($top: 1);
+  }
+  &-footer {
+    width: 100%;
+    text-align: center;
+    padding: 20px 0;
+    font-size: 12px;
+    color: #ccc;
+  }
+  &-line-left {
+    width: 50px;
+    background: #ccc;
+    display: inline-block;
+    vertical-align: middle;
     @include mix-1px($top: 1);
   }
 }
