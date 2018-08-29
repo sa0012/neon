0672a71809276e62f2cc5aec42f97c7f457b01b7
ryanxchen <javascriptchenxt@gmail.com>
2018-08-29 18:00:32 +0800

    chore: add touch-simulator

--- .postcssrc.js
+++ .postcssrc.js
@@ -6,7 +6,7 @@ module.exports = (ctx) => ({
     "postcss-url": {},
     // to edit target browsers: use "browserslist" field in package.json
     "autoprefixer": {},
-    'postcss-pxtorem': ctx.env !== 'production' ? {
+    'postcss-pxtorem': ctx.env === 'aaaproduction' ? {
       // If you modify rootValue, you also need to modify rem in static/responsive.js
       rootValue: 100, // 1rem=100px
       propWhiteList: []
