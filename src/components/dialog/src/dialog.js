314aa9544eac5d9086b1ec52df1f1a3cfff0b3a9
Ryanx Chen <ryanxchen@163.com>
2018-12-07 11:07:14 +0800

    feat: add dialog isMultiple&isInPrototype opts

--- src/components/dialog/src/dialog.js
+++ src/components/dialog/src/dialog.js
@@ -1,43 +1,73 @@
+import Vue from 'vue'
 import DialogComponent from './dialog.vue'
 
 let instance = null
+let isSingle = true
 
-const dialogPlugin = {
-  install (Vue, initOptions = {}) {
-    const dialog = {
-      show (options = {}) {
-        const Dialog = Vue.extend(DialogComponent)
-        if (!instance) {
-          instance = new Dialog().$mount(document.createElement('div'))
-          document.body && document.body.appendChild(instance.$el)
-        }
-
-        const defaults = {}
-        for (let i in instance.$options.props) {
-          if (i !== 'value') {
-            defaults[i] = instance.$options.props[i].default
-          }
-        }
-        const opt = Object.assign({}, defaults, options)
-        for (let key in opt) {
-          instance[key] = opt[key]
-        }
-        instance.visible = true
-      },
-      hide (callback) {
-        instance.visible = false
-        typeof callback === 'function' && callback()
-      },
-      alert (option) {
-        this.show(Object.assign({}, option, { type: 'alert' }))
-      },
-      confirm (option) {
-        this.show(Object.assign({}, option, { type: 'confirm' }))
-      }
+function createInstance (Vue, options) {
+  if (isSingle) {
+    if (!instance) {
+      instance = new (Vue.extend(DialogComponent))().$mount(document.createElement('div'))
+      document.body && document.body.appendChild(instance.$el)
     }
-
-    Vue.prototype.$dialog = dialog
+    margeInstanceOpts(options, instance)
+  } else {
+    const _instance = new (Vue.extend(DialogComponent))().$mount(document.createElement('div'))
+    document.body && document.body.appendChild(_instance.$el)
+    margeInstanceOpts(options, _instance)
+    return _instance
   }
 }
 
-export default dialogPlugin
+function margeInstanceOpts (options, instance) {
+  const defaults = {}
+  for (let i in instance.$options.props) {
+    if (i !== 'value') {
+      defaults[i] = instance.$options.props[i].default
+    }
+  }
+
+  const opt = Object.assign({}, defaults, options)
+  for (let key in opt) {
+    instance[key] = opt[key]
+  }
+}
+
+function addTypes (toast) {
+  const types = ['alert', 'confirm']
+  types.forEach(type => {
+    toast[type] = function (option) {
+      return toast.show(Object.assign({}, option, { type }))
+    }
+  })
+}
+
+const dialog = {
+  show (options = {}) {
+    if (isSingle) {
+      createInstance(Vue, options)
+      instance.visible = true
+    } else {
+      let _instance = createInstance(Vue, options)
+      _instance.hide = function (callback) {
+        _instance.visible = false
+        typeof callback === 'function' && callback()
+        document.body.removeChild(_instance.$el)
+      }
+      _instance.visible = true
+      return _instance
+    }
+  },
+  hide (callback) {
+    instance && (instance.visible = false)
+    typeof callback === 'function' && callback()
+  },
+  install (Vue, initOptions = { isMultiple: false, isInPrototype: true }) {
+    isSingle = !initOptions.isMultiple
+    initOptions.isInPrototype && (Vue.prototype.$dialog = dialog)
+  }
+}
+
+addTypes(dialog)
+
+export default dialog
